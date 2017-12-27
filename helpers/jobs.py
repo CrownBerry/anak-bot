1190f1b28365cc6fa8e84b976318fd24f490c1ab
Alexandr Köln <koelnfrei@gmail.com>
2017-12-27 16:51:35 +0300

    add more timeout

--- helpers/jobs.py
+++ helpers/jobs.py
@@ -15,7 +15,7 @@ def check_vk(bot, job):
         url = VkProvider().is_photo()
         group_list = MainRepository().get_group()
         for group in group_list:
-            message = bot.send_message(chat_id=group, text=text)
+            message = bot.send_message(chat_id=group, text=text, timeout=600)
             bot.pin_chat_message(chat_id=group, message_id=message.message_id)
             if url is not None:
-                bot.send_photo(chat_id=group, photo=url)
+                bot.send_photo(chat_id=group, photo=url, timeout=600)
