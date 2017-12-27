22a04a57cbf03e783ce0c9b1a7c86a204607a0b3
Alexandr Köln <koelnfrei@gmail.com>
2017-12-27 17:51:39 +0300

    change logging

--- main.py
+++ main.py
@@ -6,15 +6,14 @@ from helpers.config import Config
 from helpers.jobs import check_vk, notify
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     config = Config()
     updater = Updater(token=config.get_token())
     job_queue = updater.job_queue
     dispatcher = updater.dispatcher
 
     notify_handler = CommandHandler('notify_me', notify)
-
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
 
     job_status = job_queue.run_repeating(check_vk, interval=60, first=0)
     dispatcher.add_handler(notify_handler)
