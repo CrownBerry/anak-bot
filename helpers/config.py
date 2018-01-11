141f657e37f7e3a358f2653260eaa7fb8ca80e48
Alexandr Köln <koelnfrei@gmail.com>
2018-01-11 15:49:30 +0300

    Add os environ as alternate way to configure, for heroku

--- helpers/config.py
+++ helpers/config.py
@@ -1,15 +1,22 @@
 import configparser
+import os
 
 
 class Config:
     def __init__(self):
         config = configparser.ConfigParser()
         config.read('config.ini')
-        default_config = config['DEFAULT']
-        self.__telegram_token = default_config.get('tgToken')
-        self.__vk_token = default_config.get('vkToken')
-        self.__group = default_config.get('group')
-        self.__connection_string = default_config.get('connectionString')
+        if config.has_section('DEFAULT'):
+            default_config = config['DEFAULT']
+            self.__telegram_token = default_config.get('tgToken')
+            self.__vk_token = default_config.get('vkToken')
+            self.__group = default_config.get('group')
+            self.__connection_string = default_config.get('connectionString')
+        else:
+            self.__telegram_token = os.environ('TG_TOKEN')
+            self.__vk_token = os.environ('VK_TOKEN')
+            self.__group = os.environ('GROUP')
+            self.__connection_string = os.environ('CONNECTION_STRING')
 
     def get_token(self):
         return self.__telegram_token
